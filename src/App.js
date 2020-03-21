d3a36b911637fd5b60da336caa93208b9200392b
Kyzyl Monteiro <kyzylmonteiro@gmail.com>
2020-03-22 01:09:01 +0530

    added cards and demo json file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardActions, CardContent } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
-function App() {
+const useStyles = makeStyles({
+  app: {
+    backgroundColor: "lightgrey",
+    height: "100%"
+  },
+  card: {
+    marginRight: "10px",
+    marginLeft: "10px",
+    width: 275,
+    float: "left",
+    textAlign: "centre"
+  }
+});
+
+function App(props) {
+  const stylez = useStyles();
+  let data = props.data;
+  let cards = data.cards;
+
+  var cardsList = cards.map(card => (
+    <Card className={stylez.card} key={card.id}>
+      <CardContent>
+        <h4>
+          {card.header}
+          {card.id}
+        </h4>
+        {card.text}
+        <div>Details</div>
+      </CardContent>
+      <CardActions>
+        <Button>Back</Button>
+      </CardActions>
+    </Card>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" className={stylez.app}>
+      <h1>{data.header}</h1>
+      <h3>{data.subheader}</h3>
+      {cardsList}
     </div>
   );
 }
